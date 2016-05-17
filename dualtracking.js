9043ea1260528a1e2243c44936f0a1f532c93247
Eike Pierstorff <eike@eikes-mbp-3.speedport_w_724v_09011603_00_023>
2016-05-17 11:07:19 +0200

    added Object.keys polyfill

--- dualtracking.js
+++ dualtracking.js
@@ -10,6 +10,8 @@
  *
  *  Array methods polyfills  by  Independent software
  *  http://www.independent-software.com/about-independent-software/
+ *
+ * Object.keys polyfill from here: https://gist.github.com/jonfalcon/4715325
  */
 
 (function() {
@@ -280,6 +282,44 @@
     };
   }
 
+  /**
+   * Polyfill for Object.keys
+   *
+   * @see: https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Object/keys
+   */
+  if (!Object.keys) {
+    Object.keys = (function () {
+      var hasOwnProperty = Object.prototype.hasOwnProperty,
+          hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
+          dontEnums = [
+            'toString',
+            'toLocaleString',
+            'valueOf',
+            'hasOwnProperty',
+            'isPrototypeOf',
+            'propertyIsEnumerable',
+            'constructor'
+          ],
+          dontEnumsLength = dontEnums.length;
+
+      return function (obj) {
+        if (typeof obj !== 'object' && typeof obj !== 'function' || obj === null) throw new TypeError('Object.keys called on non-object');
+
+        var result = [];
+
+        for (var prop in obj) {
+          if (hasOwnProperty.call(obj, prop)) result.push(prop);
+        }
+
+        if (hasDontEnumBug) {
+          for (var i=0; i < dontEnumsLength; i++) {
+            if (hasOwnProperty.call(obj, dontEnums[i])) result.push(dontEnums[i]);
+          }
+        }
+        return result;
+      }
+    })()
+  };
 
   /**
    * from here http://stackoverflow.com/a/5515960/761212
